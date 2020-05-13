51bdf4c6f28a84bc8ad28a901bf75ab014c6a82c
KeithRussel <keithmaniacop@gmail.com>
2020-05-13 22:11:40 +0800

    Updates

---
+++ precache-manifest.0c54cc70727b52dcf572138fde75d9c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d675b67e516bdb86647265dc7d7a431e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "129cd191a09d0942b838",
+    "url": "/contactmanager/static/css/2.3e1a25a6.chunk.css"
+  },
+  {
+    "revision": "cabe03ec18e29a714d90",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "129cd191a09d0942b838",
+    "url": "/contactmanager/static/js/2.194c5291.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.194c5291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cabe03ec18e29a714d90",
+    "url": "/contactmanager/static/js/main.c037a455.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
